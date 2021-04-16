7e6b2a3ca0342db6fdb1f406fd6787dd1fcf1c13
Amaury Deltenre <amaury.deltenre@gmail.com>
2021-04-16 10:33:41 +0200

    Add normal gizmo

--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -7,18 +7,34 @@ public class ShootController : MonoBehaviour
 
     public ParticleSystem muzzleFlash;
     public ParticleSystem collisionExplosionPrefab;
+    private Transform _transform;
+    private RaycastHit _hitInfo;
+    private bool _isHit;
+
+    private void Start()
+    {
+        _transform = transform;
+    }
 
     private void Update()
     {
+        _isHit = Physics.Raycast(_transform.position, _transform.forward, out _hitInfo);
         if (Input.GetMouseButtonDown(0))
         {
             muzzleFlash.Play();
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo))
+            if (_isHit)
             {
-                ParticleSystem collisionExplosion = Instantiate(collisionExplosionPrefab, hitInfo.point, Quaternion.identity);
-                collisionExplosion.transform.LookAt(hitInfo.normal);
-                Destroy(hitInfo.collider.gameObject);
+                ParticleSystem collisionExplosion = Instantiate(collisionExplosionPrefab, _hitInfo.point, Quaternion.identity);
+                collisionExplosion.transform.rotation = Quaternion.LookRotation(_hitInfo.normal);
+                Destroy(collisionExplosion.gameObject, 1f);
             }
+
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_hitInfo.point, _hitInfo.point + _hitInfo.normal);
     }
 }
